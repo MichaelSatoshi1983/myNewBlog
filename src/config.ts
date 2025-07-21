3ed6931dddb9d754d3dd6897df8f639b2c948561
satoshi1983@zohomail.com <michael@localhost.localdomain>
2025-07-21 11:35:05 +0800

    first commit

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://undefined.today/", // replace this with your deployed domain
+  author: "Michael",
+  profile: "https://undefined.today/about",
+  desc: "Michael 的碎碎念。",
+  title: "undefined's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
